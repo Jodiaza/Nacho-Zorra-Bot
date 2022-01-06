7def7f0329112da81f37c6410f1cabecde369d05
Joel Esteban Diaz Arévalo <jodiaza@unal.edu.co>
2022-01-05 21:02:34 -0500

    Header done

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
+
+import { useEffect } from "react";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, fas);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ client/pages/components/Header.js
@@ -0,0 +1,84 @@
+import Styled from '@emotion/styled'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const StyledHeader = Styled.div`
+    background-color: #466B3F;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.3rem;
+    font-weight: bold;
+    .navbar {
+        a {
+            color: white;
+        }
+        button {
+            margin: 8px 0 8px 24px;
+            svg {
+                width: 32px;
+                // fill: #94B43Be;
+            }
+        }
+        div {
+            text-align: center;
+        }
+    }
+    .nacho-zorra {
+        width: 120px;
+        height: 120px;
+        border-radius: 50%;
+        background-color: #466B3F;
+        border: 3px solid white;
+        position: fixed;
+        top: -20px;
+        left: 0;
+        right: 0;
+        // bottom: 0;
+        margin: auto;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        img {
+            width: 75%;
+            height: 75%;
+        }
+    }
+    .show {
+        padding-top: 40px;
+    }
+    .navbar-nav {
+        padding: 10px 0;
+    }
+    .nav-item {
+        padding: 0 100px;
+    }
+`;
+
+const Header = () => {
+    return (
+        <StyledHeader>
+            <div className='nacho-zorra'>
+                <a href=""><img src="foxbot.png" alt="Nacho Zorra" /></a>
+            </div>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <FontAwesomeIcon icon={['fas', 'bars']} color='#94B43B' />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" aria-current="page" href="#">PROYECTO</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">CONTACTO</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </StyledHeader>
+
+    )
+}
+
+export default Header;
